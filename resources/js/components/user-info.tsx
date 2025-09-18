9a27db408aa16107ff649d389d4d9d76a042c5c9
DomicMae1 <ardon.yunors@tako.co.id>
2025-09-18 09:26:40 +0700

    feat(dashboard): show current user role on dashboard

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -4,6 +4,7 @@ import { type User } from '@/types';
 
 export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: boolean }) {
     const getInitials = useInitials();
+    const roleNames = user.roles?.map((role) => role.name).join(', ');
 
     return (
         <>
@@ -15,7 +16,10 @@ export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: 
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{user.name}</span>
-                {showEmail && <span className="text-muted-foreground truncate text-xs">{user.email}</span>}
+                <span className="text-muted-foreground truncate text-xs">
+                    {!showEmail && roleNames}
+                    {showEmail && user.email}
+                </span>
             </div>
         </>
     );
