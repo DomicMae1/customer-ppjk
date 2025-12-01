422a322e7bc1919bbc5481f3386461f08883917b
DomicMae1 <ardon.yunors@tako.co.id>
2025-12-01 14:52:07 +0700

    fix(session): correct company data handling by properly saving company id, name, and logo to session

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -69,6 +69,11 @@ class HandleInertiaRequests extends Middleware
                     );
                 },
             ],
+            'company' => [
+                'id' => session('company_id'),
+                'name' => session('company_name'),
+                'logo' => session('company_logo'), // sudah asset('storage/...') dari controller login
+            ],
             'flash' => [
                 'success' => $request->session()->get('success'),
                 'error' => $request->session()->get('error'),
