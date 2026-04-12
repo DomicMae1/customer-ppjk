c8e20351726a930b47d23bd951c912755d10309c
DomicMae1 <ardon.yunors@tako.co.id>
2026-04-13 01:15:06 +0700

    fix: change few code

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -90,7 +90,7 @@ class AuthenticatedSessionController extends Controller
         if ($company) {
             session([
                 'company_id'   => $company->id,               // ID dari tabel perusahaan
-                'company_name' => $company->nama_perusahaan,  // Nama dari tabel perusahaan
+                'company_name' => $domainRecord->nama_perusahaan,  // Nama dari tabel perusahaan
                 'company_logo' => $domainRecord->path_company_logo
                     ? asset('storage/' . $domainRecord->path_company_logo)
                     : null,
