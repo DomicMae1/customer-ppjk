4abf7aefd6eeed73fb10f4ef19273d6fdbe7d0b6
bintang-019 <tengku.bintang@tako.co.id>
2026-04-23 16:25:48 +0700

    add column customers for email

---
+++ database/migrations/user/2026_04_23_162447_add_email_to_and_email_cc_to_customers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->json('email_to')->nullable();
+            $table->json('email_cc')->nullable();
+            $table->dropColumn('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn(['email_to', 'email_cc']);
+            $table->string('email')->nullable();
+        });
+    }
+};
